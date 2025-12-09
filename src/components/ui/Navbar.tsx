c3f4d82de60fe1e35c08faf8cc3580ae93f711d0
Samo Kuyumjyan <68247946+HellLover@users.noreply.github.com>
2025-12-09 08:22:49 +0400

    Update Navbar.tsx

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -9,11 +9,6 @@ const Navbar = () => {
                     <span className='bg-gradient-to-r from-blue-600 to-blue-300 bg-clip-text text-transparent'>kuyumjyan</span>
                 </div>
                 <div className='flex row gap-2 content-center justify-center mr-5'>
-                    <a href="https://chat.kuyumjyan.xyz"
-                        className="text-sm font-bold bg-gray-700 mr-4 border border-indigo-400 rounded-md px-2 py-1 transition-transform duration-300 hover:bg-gray-800"
-                    >
-                        CHAT APP
-                    </a>
                     <a href={socials.github} target='_blank' rel='noreferrer' className='transform hover:text-violet-200'>
                         <AiFillGithub size={26} />
                     </a>
